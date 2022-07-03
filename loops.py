e54a01c56a7a1715ce0cec45bb7c099442f4fa52
shalini <106244790+kichkandi@users.noreply.github.com>
2022-07-03 15:11:53 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,964 @@
+# i=1
+# while i<=100:
+#     print(i)
+#     i+=1
+#     print(i)
+#     i+=1
+
+#*************************************************
+
+#... sum of even no..................
+
+# e = int(input("enter the num : "))
+# sum=0
+# i=0
+# while i<=e:
+#     if i%2==0:
+#         # print(i , sum)
+#         sum+=i
+#     i+=1
+# print(sum)
+
+#************************************************************
+
+#.........sum of odd no..............
+ 
+# O = int(input("enter the num : "))
+# sum = 0
+# i = 0
+# while i<=O:
+#     if i%2 >= 1:
+#         print(i , sum)
+#         sum+=i
+#     i+=1
+# print(sum)
+
+#***************************************************************
+
+#.....**.show the square.**............
+
+# s = int(input("enter the num : "))
+# i = 1
+# while i<= s:
+#     a = (i**2)
+#     print(i," = ",a)
+#     i+=1
+
+#****************************************************************
+
+ ##>>>>>.......MERAKI QUESTION......<<<<<<<<<###
+#................................................
+#q2.............
+# sum=0
+# i=1
+# while i<=100:
+#   sum=sum+i
+#   i+=1
+# print("sum of whole number : ",sum)
+
+#******************************************************
+#q3.................
+# sum=0
+# i=1
+# while i<=10:
+#   x=int(input("enter the number : "))
+#   sum=sum+x
+#   i+=1
+# print("sum of whole number : ",sum)
+
+#******************************************************
+#q4......................
+# i=1
+# while i<=100:
+#   if i%2==0:
+#     print(i*-1,end=" , ")
+#   else:
+#     print(i,end=" , ")
+#   i+=1
+
+
+
+##############################################
+#*****************************************************
+#q1....................
+# i=556
+# q=556+100
+# while i<=q:
+#   a=i-555
+#   if a%7==0:
+#     print(a)
+#   i+=1
+
+#**************************************************
+#q2........................
+# i=1
+# while i<=100:
+#   if i%3==0 and i%7==0:
+#     print("NavGurukul")
+#   elif i%3==0:
+#     print("Nav")
+#   elif i%7==0:
+#     print("Gurukul")
+#   else:
+#     print(i)
+#   i+=1
+
+#**************************************************
+#q3....its repeated..........
+#*********************************88
+#q4...................
+
+
+
+
+
+
+#***********************************************9
+#q5....................
+# a=int(input("enter how manny time we take input : "))
+# sum=0
+# i=1
+# while i<=a:
+#   b=int(input("enter the number : "))
+#   sum=sum+b
+#   i+=1
+# print("sum of total no.",sum)
+
+#**************************************************
+#q6......................
+# x =  int(input(" enter how manny time loop run : "))
+# i=1
+# while i<=x:
+#   y = int(input("enter the number : "))
+#   if y==1 or y==2:
+#     print(y,"is prime no.")
+#   elif y==3 or y==5 or y==7:
+#     print("its prime no.")
+#   elif y%2==0 or y%3==0 or y%5==0 or y%7==0:
+#     print("not prime")
+#   else :
+#     print(y,"is prime no.")
+
+#**************************************************88
+#q7...................
+#i=5
+# while i>=1:
+#   j=1
+#   while j<=5:
+#     print(i,end=" ")
+#     j+=1
+#   i-=1
+#   print(" ")
+
+#******************************************************
+######################################################
+#******************************************************8
+#q1...........
+# i=20
+# while i<=100:
+#   if i%2==0: 
+#     print(i)
+#   i+=1
+  
+#***************************************************8
+#q2...................
+# i=1
+# while i<=100:
+#   if i%7==0:
+#     print(i)
+#   i+=1
+  
+#****************************************************8
+#q3......................
+# sum=01
+# i=12
+# while i<=421:
+#   ##print(i)
+#   sum=sum+i
+#   i+=1
+# print("sum of whole number : ",sum)
+  
+#******************************************************8
+#q4.........................
+# sum=0
+# i=30
+# while i<=420:
+#   if i%8==0:
+#     sum=sum+i
+#   i+=1
+# print(sum)    
+    
+#******************************************88
+#q5........................
+# sum=0
+# i=1
+# while i<=11:
+#   x=int(input("enter the weight : "))
+#   sum=sum+x
+#   i+=1
+# print(" ")
+# print("sum : ",sum)
+# avg=sum//11
+# print("the average is: ",avg)
+# if avg%5==0:
+#   print("the average is multiple of '5'.")
+# else:
+#   print("not multiple of '5'...")
+
+#**************************************************88
+#q6........already done...........
+#***************************************************
+#q7..................
+# x=int(input("enter the number[1:10] : "))
+# i=5
+# while i>=1:
+#   y=int(input("enter the number[1:10] :"))
+#   if y>=0 and y<=10:
+#     if y==x: 
+#       print(" ")
+#       print("..wow you win the game...")
+#       break
+#     elif i==1:
+#       print(" ")
+#       print("...you loss the game...")
+#     else:
+#       print("no,its wrong")
+#       print("'now' you have",i-1,"chance more")
+#   elif i==1:
+#     print(" ")
+#     print("you loss the game.")
+#   else: 
+#     print("give us correct no.")
+#     print("'now' you have",i-1,"chance more")
+#   i-=1
+
+#****************************************************
+#q8........................
+# x=int(input("enter the num: "))
+# i=1
+# while i>0:
+#   y=int(input("enter the number : "))
+#   if y==x:
+#     print("wow you guess the correct no.")
+#     break
+#   elif y<x:
+#     print("its smaller,try one more time ")
+#   elif y>x:
+#     print("its gratter,try on more time.")
+#   i+=1
+
+#**************************************************8
+#q9............................
+# x=int(input("enter the number : "))
+# y=int(input("enter the number : "))
+# sum=0
+# i=1
+# while i<=y:
+#   sum=sum+x
+#   i+=1
+# print(sum)
+
+#************************************************88
+#q10....................
+# i=0
+# j=1
+# k=0
+# while i<=100:
+#   print(i,end=" , ")
+#   k=j
+#   j=i
+#  i=k+j
+  
+#**************************************************8
+#q .................
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###############################################################################################################
+
+# i = 0
+# while i < 3 :
+#   a = int(input("enter the number:"))
+#   print(a)
+#   i+=1
+
+#********************************
+
+# i = 0 
+# while i<3:
+#   a= int(input("enter the number:"))
+#   print(a,end=" ")
+#   i+=1
+
+#************************************
+
+# i=0
+# while i<=4:
+#   print(i)
+#   i+=1
+
+
+# i=0
+# while i<=4:
+#   i+=1
+#   print(i)
+
+# i=0
+# while i<=4:
+#   print(i)
+
+# i=0
+# while i<=4:
+#   print(i)
+#   i+=2
+
+# i=0
+# while i<=4:
+#   i+=1
+
+# i=0
+# print(i) 
+# while i<=4:
+#   i+=1
+  
+# i=1
+# while i<=4:
+#   print(i)
+#   i*=2
+
+
+# i=1
+# while i<=100:
+#   print(i)
+#   i*=2
+
+# while i<=10:
+#   i=3
+#   print(i)
+#   i+=1
+
+# sum=0
+# i=0
+# while i<=5:
+#   sum+=i
+#   i+=1
+# print(sum)
+
+
+# sum=0
+# i=0
+# while i<=5:
+#   sum+=i
+#   i+=1
+#   print(sum ,i)
+# print("total is ",sum)
+
+# sum=0
+# i=2
+# while i<=10:
+#   sum+=i
+#   i+=2
+# print(sum)
+
+#********************************************
+
+#i=1
+# while i<=100:
+#     print(i)
+#     i+=1
+#     print(i)
+#     i+=1
+
+#*************************************************
+
+#... sum of even no..................
+
+# e = int(input("enter the num : "))
+# sum=0
+# i=0
+# while i<=e:
+#     if i%2==0:
+#         # print(i , sum)
+#         sum+=i
+#     i+=1
+# print(sum)
+
+#************************************************************
+
+#.........sum of odd no..............
+ 
+# O = int(input("enter the num : "))
+# sum = 0
+# i = 0
+# while i<=O:
+#     if i%2 >= 1:
+#         print(i , sum)
+#         sum+=i
+#     i+=1
+# print(sum)
+
+#*********************************************************
+
+#.....**.show the square.**............
+
+# s = int(input("enter the num : "))
+# i = 1
+# while i<= s:
+#     a = (i**2)
+#     print(i," = ",a)
+#     i+=1
+
+#*********************************************************
+
+#.........print series.................
+
+# x = int(input("enter the num : "))
+# i = 1 
+# while i <= x:
+#     print(i*i,"+",end=" ")
+#     i+=1
+#     print(i*i,"-",end=" ")
+#     i+=1
+    
+#& & & & & & &
+
+# x= int(input("enter the num : "))
+# i = 1
+# while i <= x:
+#   if i==x:
+#     print(i*i)
+#   elif i % 2 == 0:
+#     print(i*i,"-",end=" ")
+#   else:
+#     print(i*i,"+",end=" ")
+#   i+=1
+
+#************************************************8
+
+# x = int(input("enter the number : "))
+# y = int(input("enter the number : "))
+
+# a=x/y
+# b=x//y
+# print(a)
+
+# if (a-b)<0.5:
+#   print(b)
+# elif (a-b)>=0.5:
+#   print(b+1)
+
+#***************************************************8
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#*******************************************************
+#****...........docs.............******###
+
+#q1............
+#even no.
+
+# i=1
+# while i<=20:
+#   if i%2==0:
+#     print(i)
+#   i+=1
+  
+#0dd no.
+
+# i=1
+# while i<=20:
+#   if i%2!=0:
+#     print(i)
+#   i+=1
+
+#natural no.
+
+# i=1
+# while i<=10:
+#   print(i)
+#   i+=1
+
+#whole no.
+
+# i=0
+# while i<10:
+#   print(i)
+#   i+=1
+
+#***************************************************
+#q2....................
+
+# i=1
+# while i<=10:
+#   print(i,i**2)
+#   i+=1
+
+#**********************************************
+#q3...........
+
+# i=1
+# while i<=30:
+#   print(i*10,end=" ")
+#   i+=1
+
+#************************************************
+#q4.............
+# i=105
+# while i>=7:
+#   print(i,end=" ")
+#   i-=7
+
+#************************************************
+#q5.....................
+# i=10
+# while i>=1:
+#   print(i)
+#   i-=1
+
+#**************************************************
+#q6.........................
+# sum=0
+# i=1
+# while i<=10:
+#   sum+=i
+#   i+=1
+# print(sum)
+
+#****************************************************
+#q7.........................
+# sum=0
+# i=1
+# while i<=20:
+#   if i%2==0:
+#     sum+=i
+#   i+=1
+# print(sum)
+
+#***************************************************
+#q8..................
+# x = int(input("enter the number : "))
+# i=1
+# while i<=10:
+#   print(x*i)
+#   i+=1
+
+#***************************************************
+#q9....................
+# x = int(input("enter the first number : "))
+# y = int(input("enter the second number : "))
+
+# while x<=y:
+#   if x%2==0:
+#     print(x)
+#   x+=1
+  
+#***************************************************
+#q12.......................
+# x = int(input("enter the num : "))
+# i=1
+# while x!=0:
+#   y=x%10
+#   i*=y
+#   x//=10
+# print(i)
+
+#**************************************************
+#q13....................
+
+# x = int(input("enter the number : "))
+
+# while x!=0:
+#   y=x%10
+#   print(y,end=" ")
+#   x=x//10
+
+#*************************************************
+#q15..........................
+# x = int(input("enter the num : "))
+# i=0
+# j=1
+# k=0
+# while i<=x:
+#   print(i)
+#   k=j
+#   j=i
+#   i=k+j
+
+#not know.....
+#****************************************************
+#q16..............
+# x=int(input("enter the number : "))
+# i=1
+# fact=1
+# while i<=x:
+#   fact=(fact*i)
+#   print(fact)
+#   i+=1
+  
+#******************************************************
+#q17......................
+# x=int(input("enter the number : "))
+# a=len(str(x))
+# y=x
+# s=0
+# while x!=0:
+#   m=x%10
+#   s=s+m**a
+#   x=x//10
+# print(s)
+# if s==y:
+#   print("its Armstrong number.")
+# else:
+#   print("this is not armstrong number.")
+
+#************************************************88
+#q18...................
+
+
+
+
+
+
+
+#****************************************************
+#q19..........................
+# n=int(input("enter the number : "))
+# i=1
+# while i<=n:
+#   if i==n:
+#     print('1/',i,'!')
+#   else:
+#     print('1/',i,'!'"+"," ",end=" ")
+#   i+=1
+
+#try once again:::::::
+
+#*******************************************88
+#q20.............................
+# x=int(input("enter the number : "))
+# y=x
+# z=0
+# while x!=0:
+#   a=x%10
+#   z=z*10+a
+#   x=x//10
+#   print(a,end=" ") 
+# if z==y:
+#   print("palindrome")
+# else:
+#   print("not palindrome")
+
+#********************************************8
+#q21.....................
+# x=int(input("ENTER THE NUM : "))
+# sum=0
+# i=1
+# while i<=x:
+#   sum=sum+i
+#   if i==x:
+#     print('1/',sum,'!')
+#   i+=1
+
+#*************************************************
+#q22...........................
+# i=1
+# s=0
+# while i<=10:
+#   a=int(input("enter the number : "))
+#   s=s+a
+#   i+=1
+# print(s)
+# print("average is : ",s/10)
+
+#**************************************************
+#q23.............................
+# i=0
+# while i<=10:
+  
+
+#***************************************************
+#q24....................
+# x = int(input("enter the starting number : "))
+# y = int(input("enter the last number : "))
+# sum=0
+# total=0
+# while x<=y:
+#   if x%2==0:
+#     sum =sum+x
+#    ####print("even no.-",sum)
+#   else:
+#     total = total+x
+#     ####print("num",total)
+    
+#   x+=1
+# print("sum of even no.-",sum)
+# print("sum of odd no.-",total)
+
+#************************************************8
+#q25........not in doc................
+#**********************************************
+#q26..............
+
+# num = int(input("enter the number : "))
+# i=1
+# while i<=num:
+  
+  
+
+#**************************************************
+#q27...................
+# x = int(input("enter the number : "))
+# i = 1
+# while i<=x :
+#   a = i**2
+#   print(a,end=" ")
+#   i+=1
+
+#************************************************
+#q28.......................
+
+
+
+
+
+
+
+#**************************************************
+#.........q29...is not in docs..............
+#***************************************************
+#q30........................
+# x = int(input("enter the number : "))
+# sum=0
+# i=1
+# while i<=x:
+#   y=i**3
+#   if i==x:
+#     print(y)
+#   #y=i**3
+#   else:
+#     print(y,end=" + ")
+#   sum=sum+y
+#   i+=1
+# print("sum of this series is",sum)
+  
+#************************************************
+#q31........................
+# x = int(input("inter the number : "))
+# sum=0
+# y=1
+# i=1
+# while i<=x:
+#   y=y*i
+#   if i==x:
+#     print(y)
+#   else: 
+#     print(y,end=" + ")
+#   sum=sum+y
+#   i+=1
+# print("the sum of this serise : ",sum)
+  
+#*************************************************
+#q32..................
+# x = int(input("enter the number : "))
+# sum = 0
+# i=1
+# while i<=x:
+# 	y=i**2
+# 	if i==x:
+# 	  print(y)
+# 	  sum=sum+y
+# 	elif i%2==0:
+# 	  print(y,end=" - ")
+# 	  sum=sum+y
+# 	else:
+# 	  if sum==0:
+# 	    print(y,end=" + ")
+# 	    sum=y-sum
+# 	  else: 
+# 	    print(y,end=" + ")
+# 	    sum=sum-y
+# 	i+=1
+# 	#sum=sum+y
+# print("sum of serise : ",sum)
+
+#*************************************************
+#q33.........................
+# x = input("enter the word : ")
+# i=0
+# while i<(len(x)):
+#   print(x[i])
+#   i+=1
+
+#************************************************
+#q34........................
+# L=[23,45,32,25,46,33,71,90]
+# i=0
+# while i<(len(L)):
+#   if L[i]%2 > 0:
+#     print(L[i])
+#   i+=1
+
+#***********************************************88
+#35.......................
+# x=int(input("enter the number : "))
+# sum=0
+# i=1
+# while i<=x:
+#   if x%i==0:
+#     sum=sum+i
+#     # print(i,sum)
+#   i+=1
+# print("total sum of fectors: ",sum)
+
+#************************************************8
+#q36.......................
+# x=int(input("enter the first number : "))
+# y=int(input("enter the second number : "))
+# sum=0
+# while x<=y:
+#   if x%2==0:
+#     sum=sum+x
+#   x+=1
+# print("sum of even no:",sum)
+
+#****************************************************
+#q37.........................
+# a=int(input("enter the number : "))
+# fact=1
+# i=1
+# while i<=a:
+#   fact=fact*i
+#   ###print(fact)
+#   i+=1
+# print(fact)
+
+#***********************************************
+#q38....................
+# a=1500
+# b=2700
+# while a<=b:
+#   if a%7==0 and a%5==0:
+#     print(a)
+#   a+=1
+  
+#**************************************************
+#q39.......................
+# x=1
+# y=9
+# i=1
+# while i>0:
+#   z=int(input("enter the number : "))
+#   if z>=x and z<=9:
+#     print("correct")
+#     break
+#   else:
+#     print("try one more")
+#   i+=1
+  
+#***************************************************8
+#q40........................
+
+###x=int(input("enter the first number : "))
+###y=int(input("enter the second number : "))
+
+# i=1
+# while i<=10:
+#   x=int(input("enter the first number : "))
+#   y=int(input("enter the second number : "))
+#   if x+y>=15 and x+y<=20:
+#     print(20)
+#     break
+#   else:
+#     print(x+y)
+#     print("try one more")
+#   i+=1
+
+#***************************************************
+################################################
+
+###loop 50 but output 1 to 100:########.......
+
+# a=50
+# i=1
+# k=1
+# while i<=a:
+#   j=2
+#   s=1
+#   while s<=j:
+#     print(k)
+#     s+=1
+#     k+=1
+#   i+=1
+  
+#& & & & & & & & & & & & & & & Z&
+# a=50
+# i=1
+# k=1
+# j=2
+# while i<=50:
+#   print(k)
+#   print(j)
+#   k+=2
+#   j+=2
+#   i+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
